5e1eb254e128eb3f8f5bb838ecc84bed50b17616
Vector Shady <vectorshade8080@gmail.com>
2025-10-22 14:45:46 +0500

    Operator1 is ready to use. In which i add welcome message for home screen.

---
+++ src/Operator1.py
@@ -0,0 +1,12 @@
+# Operator1.py
+
+from Operator2 import DisplayMenuTypes
+
+
+def DisplayWelcomeMessage():
+    print("\n<<<       WELCOME TO FOODIE'S PARADISE        >>>")
+    print("\n<<<  We are spreading Happiness through Food   >>>")
+    print("\n<<<  We are here to serve you the best dishes  >>>")
+    print("\n<<<============================================>>>")
+    
+    DisplayMenuTypes()
