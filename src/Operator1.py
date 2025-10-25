6f86517f6bb67ef1e4708c939480ca4d82c05805
Vector Shady <vectorshade8080@gmail.com>
2025-10-25 14:10:25 +0500

    add Comments in program.

--- src/Operator1.py
+++ src/Operator1.py
@@ -1,10 +1,12 @@
 # Operator1.py
 
+# Operator2 sy DisplayMenuTypes impoet kiya taky yaha sy hi uske function ko Call krwa sakyn.
+
 from Operator2 import DisplayMenuTypes
 
 
 def DisplayWelcomeMessage():
-    print("\n<<<       WELCOME TO FOODIE'S PARADISE        >>>")
+    print("\n<<<       WELCOME TO FOODIE'S PARADISE        >>>")     # ye sb welcome messages hain.
     print("\n<<<  We are spreading Happiness through Food   >>>")
     print("\n<<<  We are here to serve you the best dishes  >>>")
     print("\n<<<============================================>>>")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 # main.py
 
+'''Operator1 sy DisplayWelcomeMessage import krwa rahy hain taky main screen pr Welcome maessage print kr sakyn.'''
+
 from Operator1 import DisplayWelcomeMessage
 
 
